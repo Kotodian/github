6440f94c42372198bcbda4427b2a5d38bd5ca199
Kotodian <417350372@qq.com>
2018-11-07 10:41:47 +0800

    jquery init

---
+++ jquery_init.js
@@ -0,0 +1,13 @@
+var $=jQuery=function(x){
+    return jQuery.fn.init(x);
+}
+jQuery.fn=jQuery.prototype={
+    init:function(x){
+        this.num=x||0;
+        return this;
+    },
+    jquery:"1.11"
+}//jQuery.fn相当于jQuery.prototype的别名
+console.log(jQuery.fn.init.prototype);
+jQuery.fn.init.prototype=jQuery.fn;//为了让jQuery.fn.init这个工厂方法能够继承jQuery.fn上的原型方法,让返回对象为jQuery.fn
+console.log(jQuery.fn.init.prototype);
